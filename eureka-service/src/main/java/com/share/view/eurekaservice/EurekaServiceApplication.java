fe4115bbc63fba9a7a0bd7e9b5319cbfa521abb6
vigneshtj <vignesh.janarthanan@gmail.com>
2019-04-10 20:05:39 +0530

    commiting with zuul

--- eureka-service/src/main/java/com/share/view/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/share/view/eurekaservice/EurekaServiceApplication.java
@@ -3,7 +3,9 @@ package com.share.view.eurekaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
